b03882c5fe90fcb269dbef1479f138605fad1b50
Tia1116 <1813289@nzse.ac.nz>
2021-05-25 17:16:56 +1200

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: true }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
