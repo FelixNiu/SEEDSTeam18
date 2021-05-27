3e53de52096fc0bbb482dc623057aa82fe020f07
Tia1116 <1813289@nzse.ac.nz>
2021-05-27 19:54:11 +1200

    update test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-const ShowArticleList = require('./components/ShowArticleList');
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-
-describe('<ShowArticleList />', () => {
-    const wrapper = shallow(<ShowArticleList />);
-    it('it should find 6 div tags', () => {
-        expect(wrapper.find('div')).tobe(6);
-    });
-
-    it('should find 1 h1 tag', () => {
-        expect(wrapper.find('h1')).tobe(1);
-    });
-    it('should find 2 Link tags', () => {
-        expect(wrapper.find('Link')).tobe(2);
-    });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
