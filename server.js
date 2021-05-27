e442c6bf86509ccbbe39edf48b13f1924a2a8fd0
Tia1116 <1813289@nzse.ac.nz>
2021-05-27 12:44:12 +1200

    heroku test 8

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
-const path=require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
+const path = require('path');
 
 // routes
-const articles = require('./routes/api/articles');
+const articles = require("./routes/api/articles");
 
 const app = express();
 
@@ -15,15 +15,22 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.json({ extended: false }));
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', './client/public/index.html'));
-});
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname,"client","build","index.html"));
+  });
+} else{
+  app.get("/", (req,res) =>{
+      res.send("api running");
+  });
+}
 
 // use Routes
-app.use('/api/articles', articles);
+app.use("/api/articles", articles);
 
 const port = process.env.PORT || 5000;
 
